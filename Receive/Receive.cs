fff41cfc72011887290f983a78ac7951e0f28ae1
tebeco <tebeco@gmail.com>
2019-01-26 16:57:13 +0100

    01: Handle message sending + cadence

--- Receive/Receive.cs
+++ Receive/Receive.cs
@@ -1,12 +1,45 @@
 ﻿using System;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
 
 namespace Receive
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static async Task Main()
         {
-            Console.WriteLine("Hello World!");
+            var cts = new CancellationTokenSource();
+            Console.CancelKeyPress += Console_CancelKeyPress;
+            var ct = cts.Token;
+
+            var connectionFactory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = connectionFactory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: "hello", durable: false, exclusive: false, autoDelete: false, arguments: null);
+
+                var consumer = new EventingBasicConsumer(channel);
+                consumer.Received += (model, ea) =>
+                {
+                    var body = ea.Body;
+                    var message = Encoding.UTF8.GetString(body);
+                    Console.WriteLine(" [x] Received {0}", message);
+                };
+                channel.BasicConsume(queue: "hello", autoAck: true, consumer: consumer);
+
+
+                await Task.Delay(-1, ct).ContinueWith(tsk => { });
+            }
+
+            void Console_CancelKeyPress(object sender, ConsoleCancelEventArgs e)
+            {
+                e.Cancel = true;
+                cts.Cancel();
+            }
+
         }
     }
 }
--- Send/Send.cs
+++ Send/Send.cs
@@ -1,12 +1,70 @@
 ﻿using System;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
 
 namespace Send
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static async Task Main()
         {
-            Console.WriteLine("Hello World!");
+            var cts = new CancellationTokenSource();
+            Console.CancelKeyPress += Console_CancelKeyPress;
+            var ct = cts.Token;
+
+            var connectionFactory = new ConnectionFactory { HostName = "localhost" };
+            using (var connection = connectionFactory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: "hello", durable: false, exclusive: false, autoDelete: false, arguments: null);
+
+                var itemIndex = 0;
+                var delay = TimeSpan.FromMilliseconds(700);
+                var delta = TimeSpan.FromMilliseconds(20);
+                while (!ct.IsCancellationRequested)
+                {
+                    string message = $"[{itemIndex++:d3}] Hello World!";
+                    var body = Encoding.UTF8.GetBytes(message);
+
+                    channel.BasicPublish(exchange: "", routingKey: "hello", basicProperties: null, body: body);
+                    Console.WriteLine(" [x] Sent {0}", message);
+
+
+
+
+
+
+
+                    await Task.Delay(delay, ct).ContinueWith(tsk => { });
+                    while (Console.KeyAvailable)
+                    {
+                        var key = Console.ReadKey(false);
+                        switch (key.Key)
+                        {
+                            case ConsoleKey.UpArrow:
+                                delay = delay.Add(delta);
+                                break;
+                            case ConsoleKey.DownArrow:
+                                delay = delay.Subtract(delta);
+                                break;
+                        }
+
+                        if (delay <= delta)
+                            delay = delta;
+                        Console.WriteLine($"New delay between messge: {delay.TotalMilliseconds}ms");
+                    }
+                }
+            }
+
+            void Console_CancelKeyPress(object sender, ConsoleCancelEventArgs e)
+            {
+                e.Cancel = true;
+                cts.Cancel();
+            }
         }
+
     }
 }
